bdfba44a32dc37a526df0f4b6065a9e8a60e4d8c
Maletta <smaletta@hotmail.com>
2025-03-02 07:48:56 -0800

    Update 6
    Added preview text for each note to be displayed on main user page so it's not too cluttered with large notes.

--- models/Note.js
+++ models/Note.js
@@ -7,11 +7,20 @@ const noteSchema = new mongoose.Schema({
     content: {
         type: String, required: true, trim: true
     },
+    preview: {
+        type: String, trim: true
+    },
     user: {
         type: mongoose.Schema.Types.ObjectId, ref: 'User',  required: true
     }
     
 }, {timestamps: true});
+
+// pre middleware to save preview text from content
+noteSchema.pre('save', function (next) {
+    this.preview = this.content.substring(0, 100);
+    next();
+});
 
 const Note = mongoose.model('Note', noteSchema);
 
